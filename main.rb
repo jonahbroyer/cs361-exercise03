da7150bf1ea2db73be5b0cbfcdc4dbd131e55196
Jonah Broyer <jkbsk8er@yahoo.com>
2021-10-13 11:32:03 -0700

    Changed if statement to be on one line.

--- main.rb
+++ main.rb
@@ -3,15 +3,12 @@ class Student
 
   def add_to_schedule(course, schedule, quarter_id)
     quarter = schedule.quarter[quarter_id]
-    course_list = quarter.course_list
-    max_courses = course_list.maximum_number_of_courses
-    if max_courses < MAX_COURSES
-      course_list.add(course)
-    end
+    course_list_for_quarter = quarter.course_list
+    max_courses = course_list_for_quarter.maximum_number_of_courses
+    course_list_for_quarter.add(course) if max_courses < MAX_COURSES
   end
 
   def remove_from_schedule(course, schedule, quarter_id)
     schedule.quarter[quarter_id].course_list.remove(course)
   end
-
 end
